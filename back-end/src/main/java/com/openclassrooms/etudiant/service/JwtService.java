4be7e0eefaa4995489b19e16408a76f4c20504a2
Mikael Fourré <fmikl@outlook.fr>
2026-06-13 09:32:13 +0200

    Fix user authentication and implement JWT generation

--- back-end/src/main/java/com/openclassrooms/etudiant/service/JwtService.java
+++ back-end/src/main/java/com/openclassrooms/etudiant/service/JwtService.java
@@ -1,14 +1,41 @@
 package com.openclassrooms.etudiant.service;
 
 
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
     public String generateToken(UserDetails userDetails) {
-        return null; // TODO
-    }
+        Instant now = Instant.now();
+
+        SecretKey key = Keys.hmacShaKeyFor(
+            secret.getBytes(StandardCharsets.UTF_8)
+        );
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusMillis(expiration)))
+                .signWith(key)
+                .compact();
+        }
 
 }
